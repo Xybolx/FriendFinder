722dbc0a9d2588ad51eec72d5bfe54367c30d61f
Xybolx <xybolx@gmail.com>
2019-02-18 09:14:51 -0700

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+{
+     name:"Trevor",
+     photo:"https://icon2.kisspng.com/20180325/loq/kisspng-grand-theft-auto-v-grand-theft-auto-vice-city-sto-dishonoured-5ab854c6be4871.1439451615220297667794.jpg",
+     scores:[
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+     name:"Franklin",
+     photo:"https://cdn.sc.rockstargames.com/ext/n/img/char/char_frank.png",
+     scores:[
+        "5",
+        "2",
+        "3",
+        "4",
+        "4",
+        "2",
+        "2",
+        "1",
+        "5",
+        "3"
+      ]
+    },
+    {
+     name:"Michael",
+     photo:"https://image.myanimelist.net/ui/y75Sy0I831U7NNCH98jWCyj8twlhHyD7sOKZNw5_xWyLzI2FYqr6uezsfpX792UYECcTJqSmyrnqnHO4Qxui6A",
+     scores:[
+        "3",
+        "1",
+        "3",
+        "3",
+        "5",
+        "1",
+        "2",
+        "1",
+        "5",
+        "4"
+      ]
+  }
+]; 
+
+// export friends object
+module.exports = friends;
