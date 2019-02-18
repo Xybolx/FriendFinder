60a7cd275f60ffc1bbbede9f27d64f2febe3cc8c
Xybolx <xybolx@gmail.com>
2019-02-18 10:20:37 -0700

    "I hate heroku"

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var port = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -19,7 +19,7 @@ require("./app/routing/htmlRoutes")(app);
 
 
 
-app.listen(port, function() {
-    console.log("App listening on PORT: " + port);
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
   });
   
