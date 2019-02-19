d5711b743b02cd2c454956cbf9528da30c9d5f5b
Xybolx <xybolx@gmail.com>
2019-02-18 17:21:09 -0700

    "added amanda, tracey and jimmy"

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,53 +1,118 @@
 var friends = [
-{
-     name:"Trevor",
-     photo:"https://icon2.kisspng.com/20180325/loq/kisspng-grand-theft-auto-v-grand-theft-auto-vice-city-sto-dishonoured-5ab854c6be4871.1439451615220297667794.jpg",
-     scores:[
-        "5",
-        "1",
-        "4",
-        "4",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
-      ]
-    },
-    {
-     name:"Franklin",
-     photo:"https://cdn.sc.rockstargames.com/ext/n/img/char/char_frank.png",
-     scores:[
-        "5",
-        "2",
-        "3",
-        "4",
-        "4",
-        "2",
-        "2",
-        "1",
-        "5",
-        "3"
-      ]
-    },
-    {
-     name:"Michael",
-     photo:"https://image.myanimelist.net/ui/y75Sy0I831U7NNCH98jWCyj8twlhHyD7sOKZNw5_xWyLzI2FYqr6uezsfpX792UYECcTJqSmyrnqnHO4Qxui6A",
-     scores:[
-        "3",
-        "1",
-        "3",
-        "3",
-        "5",
-        "1",
-        "2",
-        "1",
-        "5",
-        "4"
-      ]
+  {
+    name: "Trevor",
+    photo: "https://pngimage.net/wp-content/uploads/2018/06/gta-v-personajes-png-5.png",
+    scores: [
+      "5",
+      "1",
+      "2",
+      "2",
+      "1",
+      "5",
+      "5",
+      "1",
+      "1",
+      "1"
+    ]
+  },
+  {
+    name: "Franklin",
+    photo: "https://cdn.sc.rockstargames.com/ext/n/img/char/char_frank.png",
+    scores: [
+      "5",
+      "1",
+      "5",
+      "2",
+      "2",
+      "2",
+      "3",
+      "2",
+      "2",
+      "2"
+    ]
+  },
+  {
+    name: "Michael",
+    photo: "https://image.myanimelist.net/ui/y75Sy0I831U7NNCH98jWCyj8twlhHyD7sOKZNw5_xWyLzI2FYqr6uezsfpX792UYECcTJqSmyrnqnHO4Qxui6A",
+    scores: [
+      "3",
+      "5",
+      "3",
+      "2",
+      "4",
+      "4",
+      "3",
+      "4",
+      "1",
+      "3"
+    ]
+  },
+  {
+    name: "Jimmy",
+    photo: "http://images2.wikia.nocookie.net/__cb20130430170633/gtawiki/images/a/a1/Jimmy-GTA5.png",
+    scores: [
+      "5",
+      "1",
+      "1",
+      "1",
+      "4",
+      "3",
+      "3",
+      "1",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Ron",
+    photo: "https://img.gta5-mods.com/q95/images/gta-character-artwork-for-decal5/d52ff0-Ron.png",
+    scores: [
+      "5",
+      "5",
+      "1",
+      "2",
+      "1",
+      "2",
+      "3",
+      "4",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Amanda",
+    photo: "https://vignette.wikia.nocookie.net/eatl/images/b/ba/Amanda_De_Santa.png/revision/latest?cb=20161025124107",
+    scores: [
+      "1",
+      "1",
+      "2",
+      "4",
+      "3",
+      "4",
+      "2",
+      "4",
+      "1",
+      "2"
+    ]
+  },
+  {
+    name: "Tracey",
+    photo: "https://i.pinimg.com/originals/f7/b5/bf/f7b5bf0f6b6d295148b81f9922018d63.png",
+    scores: [
+      "1",
+      "1",
+      "2",
+      "2",
+      "1",
+      "1",
+      "2",
+      "1",
+      "1",
+      "1"
+    ]
   }
-]; 
+  
+];
 
 // export friends object
 module.exports = friends;
